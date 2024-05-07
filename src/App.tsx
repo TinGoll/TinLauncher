d444c14a7992024e5ad5bbf821165c1277fe506b
Roman <roma.oliynyk83@gmail.com>
2024-05-07 21:58:59 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC, useState } from 'react'
+import UpdateElectron from '@/components/update'
+import logoVite from './assets/logo-vite.svg'
+import logoElectron from './assets/logo-electron.svg'
+import './App.css'
+
+export const App: FC = () => {
+  return (
+    <div>
+      Привет
+    </div>
+  )
+}
